ce8b452e2cdc78b3ae76224f25f1b30309d9e25a
Kawaljeet Singh <kawaljeet.singh@successive.com>
2019-09-05 18:09:03 +0530

    pattern

---
+++ Pattern.java
@@ -0,0 +1,14 @@
+public class Pattern
+{
+    public static void main(String[] args)
+    {
+        for(int i=1;i<=3;i++)
+        {
+            for(int j=0;j<8;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
