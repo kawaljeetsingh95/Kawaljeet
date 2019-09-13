c4f915d224523b909b556b7143a68dd82a6e3fa2
shivanimangla_successive <kawaljeet.singh@successive.com>
2019-09-13 18:38:28 +0530

    java1

---
+++ Abstract_demo.java
@@ -0,0 +1,12 @@
+public class Abstract_demo
+{
+    public static void main(String[] args) {
+
+        Abstraction f = new Area2();
+        f.rectangle();
+        f.square();
+        f.demo();
+    }
+
+}
+
